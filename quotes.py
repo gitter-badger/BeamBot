572227856730e95054813b5b2ebe0979bfee5861
RPiAwesomeness <aeves.nate@gmail.com>
2015-10-23 16:13:42 -0400

    Working on quote system

--- quotes.py
+++ quotes.py
@@ -1,11 +1,11 @@
 import usage
 import sqlite3
 
-def addQuote(user_name, split, is_mod, is_owner):
+def addQuote(user, split, is_mod, is_owner):
 
     # The user is the first item after !quote add
     if len(split) == 1:	# It's just a username, anything more indicates an incorrect command
-        return usage.prepCmd(user_name, "quote", is_mod, is_owner)
+        return usage.prepCmd(user, "quote", is_mod, is_owner)
 
     elif len(split) >= 2:
         # The quote is the second item(s) in the list
@@ -25,9 +25,9 @@ def addQuote(user_name, split, is_mod, is_owner):
             return None
 
     else:
-        return usage.prepCmd(user_name, "quote", is_mod, is_owner)
+        return usage.prepCmd(user, "quote", is_mod, is_owner)
 
-def removeQuote(user_name, split, is_mod, is_owner):
+def removeQuote(user, split, is_mod, is_owner):
 
     # split is set to equal everything after the !quote add/remove
     split = split[2:]
@@ -37,13 +37,13 @@ def removeQuote(user_name, split, is_mod, is_owner):
         user = user[1:]	# Remove the @ sign, we work without them
 
     if len(split) == 1:	# It's just a username, anything more indicates an incorrect command
-        return usage.prepCmd(user_name, "quote", is_mod, is_owner)
+        return usage.prepCmd(user, "quote", is_mod, is_owner)
 
     elif len(split) >= 2:
         quote_id = split[1]
 
         command = """DELETE FROM quotes
-                        WHERE id LIKE quote_id"""
+                        WHERE id LIKE {}""".format(quote_id)
 
         if os.path.exists('data/beambot.sqlite'):
             with sqlite3.connect('data/beambot.sqlite') as con:
@@ -53,4 +53,4 @@ def removeQuote(user_name, split, is_mod, is_owner):
             return None
 
     else:
-        return usage.prepCmd(user_name, "quote", is_mod, is_owner)
+        return usage.prepCmd(user, "quote", is_mod, is_owner)
