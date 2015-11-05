5ba0b3d5b06dee224e993aa97f8f7c54e563d133
Nathanael Olander <aeves.nate@gmail.com>
2015-11-05 00:19:18 -0500

    Added GPL Affero license notice & LICENSE.txt file

--- beambot.py
+++ beambot.py
@@ -1,3 +1,20 @@
+"""
+	This file is part of PyBot,
+	PyBot(c) RPiAwesomeness 2015-2016
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/agpl.html>.
+"""
 #!/usr/bin/env python3
 """
 -+=============================================================+-
@@ -8,8 +25,8 @@
 	Changelog:	Fixed a bug where a message of just !
 				would crash the bot
 			Fixed a bug where running currency cmds
-				within the cmd timeout would 
-				crash the bot	
+				within the cmd timeout would
+				crash the bot
 -+=============================================================+
 """
 
